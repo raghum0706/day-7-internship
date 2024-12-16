90b0a428206cc79f675e8329a7ca8bfb51890f06
Raghu M <raghumoorthy0706@gmail.com>
2024-12-16 15:51:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function msg(){
+    alert("Here is the bug")
+}
+function code(){
+    var v=confirm("are you sure?");
+    if(v==true){
+        alert("ok");
+    }
+    else{
+        alert("cancel");
+    }
+}
+function mona(){
+    var v=prompt("who are you?");
+        alert("i am "+v);
+}
